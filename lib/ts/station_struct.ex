cea3490903272a23505145d0164809f013d5dbdc
ahaldar <aparajitah@gmail.com>
2016-08-28 10:58:24 +0530

    Added StationStruct to store state, closes #3

--- lib/ts/station_struct.ex
+++ lib/ts/station_struct.ex
@@ -3,5 +3,3 @@ defmodule StationStruct do
     schedule: [%{vehicleID: nil, src_station: nil, dst_station: nil, dept_time: nil, arrival_time: nil, mode_of_transport: nil}],
     congestion_low: "delay * 2", congestion_high: "delay * 3"
 end
-
-
