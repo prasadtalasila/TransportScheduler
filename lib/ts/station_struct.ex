7b185bf07249473f94bd5ccaa7d6a91d6ac42789
Suhas Pai <suhasspai@users.noreply.github.com>
2016-08-29 01:05:33 +0530

    Added PID (Process ID) field to struct

--- lib/ts/station_struct.ex
+++ lib/ts/station_struct.ex
@@ -2,5 +2,5 @@ defmodule StationStruct do
   defstruct locVars: %{delay: nil, congestionDelay: nil, congestion: nil, disturbance: nil},
     schedule: [%{vehicleID: nil, src_station: nil, dst_station: nil, dept_time: nil, arrival_time: nil, mode_of_transport: nil}],
     congestion_low: "delay * 2", congestion_high: "delay * 3",
-    station_number: 0, station_name: "none"
+    station_number: 0, station_name: "none", pid: pid("0.0.0")
 end
