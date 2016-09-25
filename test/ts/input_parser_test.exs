14f74e8fc7a655e3022cdfb41543ff2f9bb8224f
Suhas Pai <suhasspai@users.noreply.github.com>
2016-09-25 17:03:00 +0530

    Updated with correct test values

--- test/ts/input_parser_test.exs
+++ test/ts/input_parser_test.exs
@@ -8,10 +8,13 @@ defmodule InputParserTest do
     code=InputParser.get_city_code(pid, "Alnavar Junction")
     assert code==5
     assert InputParser.get_local_variables(pid, code)==%{congestion: "low", delay: 0.22, disturbance: "no"}
-    #assert InputParser.get_schedule(pid, code)==[%{arrival_time: 63300, dept_time: 300, dst_station: 7, mode_of_transport: "train", src_station: 5, vehicleID: 19019}]
-    assert InputParser.get_station_struct(pid, "Alnavar Junction")==%StationStruct{congestion_high: "delay * 3", congestion_low: "delay * 2",
-    locVars: %{congestion: "low", congestionDelay: nil, delay: 0.22, disturbance: "no"}, schedule: [%{arrival_time: 63300, dept_time: 300, 
-    dst_station: 7, mode_of_transport: "train", src_station: 5, vehicleID: 19019}], station_name: "Alnavar Junction", station_number: 5}
+    assert InputParser.get_schedule(pid, code)==[%{arrival_time: 45000, dept_time: 900, dst_station: 2, mode_of_transport: "train", src_station: 5, vehicleID: 11043},
+    %{arrival_time: 63300, dept_time: 300, dst_station: 7, mode_of_transport: "train", src_station: 5, vehicleID: 19019}]
+    assert InputParser.get_station_struct(pid, "Alnavar Junction")==%StationStruct{choose_fn: nil, congestion_high: nil, congestion_low: nil, 
+    locVars: %{congestion: "low", congestionDelay: nil, delay: 0.22, disturbance: "no"}, pid: nil, schedule: [%{arrival_time: 45000, dept_time: 900, dst_station: 2,
+    mode_of_transport: "train", src_station: 5, vehicleID: 11043}, %{arrival_time: 63300, dept_time: 300, dst_station: 7, mode_of_transport: "train", src_station: 5, 
+    vehicleID: 19019}], station_name: "Alnavar Junction", station_number: 5}
+    InputParser.stop(pid)
   end
   
 end
