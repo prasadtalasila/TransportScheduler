96e619481fd98d87c61a86ae05ae06ac472cc208
prasadtalasila <tsrkp@goa.bits-pilani.ac.in>
2017-08-27 05:15:11 +0530

    api_supervisor removed and data/times.csv added

--- lib/ts/supervisor.ex
+++ lib/ts/supervisor.ex
@@ -18,6 +18,12 @@ defmodule TS.Supervisor do
 	a supervisor specification which can be created	with the help of the
 	functions in the Supervisor.Spec module.
 
+	###Function of Supervisor
+	The module TS.Supervisor is the top level supervisor for the project. The
+	children of a Supervisor can be workers or supervisors. TS.Supervisor spawns
+	three children, the Network Constructor (worker), the API (worker)
+	and a Station Supervisor (Supervisor).
+
 	### Parameters
 	For TS, required parameters passed to Supervisor function are
 	`Supervisor.start_link(__MODULE__, :ok)`, specifying:
@@ -25,6 +31,16 @@ defmodule TS.Supervisor do
 	module_name
 
 	args
+
+	options:
+	- :name - used for name registration
+	- :timeout - if present, the server is allowed to spend the given amount
+	of milliseconds initializing or it will be terminated and the start function
+	will return {:error, :timeout}
+	- :debug - if present, the corresponding function in the :sys module is
+	invoked
+	- :spawn_opt - if present, its value is passed as options to the underlying
+	process
 
 	### Strategies
 	Supervisors support different supervision strategies (through the
@@ -64,10 +80,49 @@ defmodule TS.Supervisor do
 		Supervisor.start_link(__MODULE__, :ok)
 	end
 
+	@doc """
+	Dynamically adds a child specification to supervisor and starts that child.
+	child_spec should be a valid child specification (unless the supervisor is a
+	:simple_one_for_one supervisor). The child process will be started as defined
+	in the child specification.
+
+	In the case of :simple_one_for_one, the child specification defined in the
+	supervisor is used and instead of a child_spec, an arbitrary list of terms is
+	expected. The child process will then be started by appending the given list
+	to the existing function arguments in the child specification.
+
+	### Parameters
+	For API, required parameters passed to Supervisor function are
+	`Supervisor.start_child(@name, [])`, specifying:
+
+	supervisor_name
+
+	child_specs_or_args
+
+	### Return values
+	If a child specification with the specified id already exists, child_spec is
+	discarded and this function returns an error with :already_started or
+	:already_present if the corresponding child process is running or not,
+	respectively.
+
+	If the child process start function returns {:ok, child} or {:ok, child, info},
+	then child specification and PID are added to the supervisor and this function
+	returns the same value.
+
+	If the child process start function returns :ignore, the child specification is
+	added to the supervisor, the PID is set to :undefined and this function returns
+	{:ok, :undefined}.
+
+	If the child process start function returns an error tuple or an erroneous
+	value, or if it fails, the child specification is discarded and this function
+	returns	{:error, error} where error is a term containing information about
+	the error and child specification.
+	"""
+
 	def init(:ok) do
 		children=[
 			worker(NetworkConstructor, [NetworkConstructor]),
-			supervisor(TS.API.Supervisor, []),
+			worker(API, []),
 			supervisor(TS.Station.Supervisor, [])
 		]
 		supervise(children, strategy: :rest_for_one)
