ae44526dd7002a104dc42ca0be002c6a3c225b63
Vedang <f2015028@goa.bits-pilani.ac.in>
2017-11-15 23:38:53 +0530

    Added deps and basic station_behaviour

---
+++ lib/ts/station_behaviour.ex
@@ -0,0 +1,8 @@
+defmodule StationBehaviour do
+	@callback get_vars(pid) :: {:next_state, term, term, term}
+	@callback get_state(pid) :: {:next_state, term, term, term}
+	@callback update(pid, struct) :: {:next_state, term, term} 
+	@callback receive_at_src(pid, pid, list) :: {:ok}
+	@callback send_to_stn(pid, pid, pid) :: {:ok}
+	@callback check_neighbours(list, list, number, list) :: list
+end
