4abb3c3daf4d317b5482bbe4bc462f42d92e9540
Suhas Shripad Hebbar <f20150163@goa.bits-pilani.ac.in>
2018-01-20 18:20:04 +0530

    Functioning Station module

--- mix.exs
+++ mix.exs
@@ -5,12 +5,11 @@ defmodule TransportScheduler.Mixfile do
     [
       app: :TransportScheduler,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.5.3",
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [coveralls: :test],
       source_url: "https://github.com/prasadtalasila/TransportScheduler",
       name: "TransportScheduler",
-
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -26,9 +25,11 @@ defmodule TransportScheduler.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mox, "~> 0.3.0", only: :test},
+      {:mox, "~> 0.3.1", only: :test},
       {:excoveralls, "~> 0.7", only: :test},
-      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:fsm, "~> 0.3.0"}
+      #{:workex, "~> 0.10.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
