c266f4b9fccf741474fc54cdb19aa4ab0a5b5361
Suhas Hebbar <suhasshripadhebbar@gmail.com>
2018-02-01 02:06:38 +0530

    StationFsm code refactored away from station.ex

--- lib/ts/station_behaviour.ex
+++ lib/ts/station_behaviour.ex
@@ -2,7 +2,7 @@ defmodule StationBehaviour do
 	@moduledoc """
 	Defines the interface of a Station.
 	"""
-	@callback get_vars(pid) :: {:next_state, term, term, term}
-	@callback get_state(pid) :: {:next_state, term, term, term}
-	@callback update(pid, struct) :: {:next_state, term, term}
+	@callback update(pid, struct) :: any()
+	@callback get_timetable(pid()) :: any()
+
 end
