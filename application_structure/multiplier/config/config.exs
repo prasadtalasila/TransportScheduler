0aad040f37de06b5eee09f3a691b67daadc21b81
bgargi <gargi.solaria@gmail.com>
2018-03-29 13:33:42 +0530

    Adding IO statements in config files

--- application_structure/multiplier/config/config.exs
+++ application_structure/multiplier/config/config.exs
@@ -1,6 +1,7 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
+IO.puts "Loading from config/config.exs in multiplier."
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
