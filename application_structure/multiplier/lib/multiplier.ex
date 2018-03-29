f626ffd6dccfc4c69e46767320db7abba054bcfa
bgargi <gargi.solaria@gmail.com>
2018-03-29 13:38:26 +0530

    Update multiplier.ex

--- application_structure/multiplier/lib/multiplier.ex
+++ application_structure/multiplier/lib/multiplier.ex
@@ -4,7 +4,7 @@ defmodule Multiplier do
         use GenServer
     
         def start_link(_) do
-                GenServer.start_link(__MODULE__,:ok, [debug: [:statistics, :trace]])
+                GenServer.start_link(__MODULE__,:ok)
         end
 
     
