64508abfb863608f56261e638330e68cf47007d0
bgargi <gargi.solaria@gmail.com>
2018-03-29 12:08:53 +0530

    Adding application structure (#103)
    adds example on mix config and application structure

---
+++ application_structure/multiplier/mix.exs
@@ -0,0 +1,33 @@
+defmodule Multiplier.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :multiplier,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Multiplier.Application,[]} 
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+
+
+  defp deps do
+    [
+
+      {:input_parser,  path: "/home/bgargi/Videos/input_parser"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+end
