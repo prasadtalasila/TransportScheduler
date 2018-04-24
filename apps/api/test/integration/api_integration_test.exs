b2a8d7e8359d87baccdde938ed041bb51d2ab915
Surya Pratap Mehra <mehrasuryapratap8@gmail.com>
2018-04-24 20:53:33 +0530

    Separate unit and integration tests

---
+++ apps/api/test/integration/api_integration_test.exs
@@ -0,0 +1,8 @@
+defmodule ApiIntegrationTest do
+  use ExUnit.Case
+  doctest Api
+
+  test "greets the world" do
+    assert Api.hello() == :world
+  end
+end
---
+++ apps/transport_scheduler/test/integration/transport_scheduler_integration_test.exs
@@ -0,0 +1,8 @@
+defmodule TransportSchedulerIntegrationTest do
+  use ExUnit.Case
+  doctest TransportScheduler
+
+  test "greets the world" do
+    assert TransportScheduler.hello() == :world
+  end
+end
