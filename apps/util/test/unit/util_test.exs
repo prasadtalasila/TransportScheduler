5067a986653befebaf1a5f055b6ce95d2ee81a4c
bgargi <gargi.solaria@gmail.com>
2018-09-30 09:58:15 +0530

    Fixing build

---
+++ apps/util/test/unit/util_test.exs
@@ -0,0 +1,4 @@
+defmodule UtilTest do
+  use ExUnit.Case
+  doctest Util
+end
--- apps/util/test/util_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule UtilTest do
-  use ExUnit.Case
-  doctest Util
-
-  test "greets the world" do
-    assert Util.hello() == :world
-  end
-end
